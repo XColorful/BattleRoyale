304875643eb62e021755d5b9265d143dc2f33522
XiaoColorful <XColor_ful@outlook.com>
2025-06-27 12:26:06 +0800

    Bot config placeholder

--- src/main/java/xiao/battleroyale/api/ConfigEntryTag.java
+++ src/main/java/xiao/battleroyale/api/ConfigEntryTag.java
@@ -3,7 +3,8 @@ package xiao.battleroyale.api;
 public class ConfigEntryTag {
 
     public static String TYPE_NAME = "type";
+    public static String ID = "id";
     public static final String DEFAULT = "default";
     public static String NAME = "name";
-    public static String COLOr = "color";
+    public static String COLOR = "color";
 }
