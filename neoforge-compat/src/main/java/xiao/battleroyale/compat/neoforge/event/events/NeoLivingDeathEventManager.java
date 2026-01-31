2cd2fc9463ea6c4f6bf1fe2d86754a9e4b8c4e51
XiaoColorful <XColor_ful@outlook.com>
2026-01-31 23:49:00 +0800

    Port isomorphic event refactor to neoforge

--- neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/events/NeoLivingDeathEventManager.java
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/events/NeoLivingDeathEventManager.java
@@ -1,31 +1,87 @@
 package xiao.battleroyale.compat.neoforge.event.events;
 
+import net.neoforged.bus.api.Event;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
 import xiao.battleroyale.api.event.EventPriority;
+import xiao.battleroyale.api.event.EventType;
 import xiao.battleroyale.api.event.IEventHandler;
+import xiao.battleroyale.compat.neoforge.event.NeoEvent;
+import xiao.battleroyale.compat.neoforge.event.NeoLivingDeathEvent;
 
 public class NeoLivingDeathEventManager {
 
     public static boolean register(IEventHandler eventHandler, EventPriority priority, boolean receiveCanceled) {
-        boolean registered = false;
-        switch (priority) {
-            case HIGHEST -> registered = NeoLivingDeathHighest.get().addEventHander(eventHandler, receiveCanceled);
-            case HIGH -> registered = NeoLivingDeathHigh.get().addEventHander(eventHandler, receiveCanceled);
-            case NORMAL -> registered = NeoLivingDeathNormal.get().addEventHander(eventHandler, receiveCanceled);
-            case LOW -> registered = NeoLivingDeathLow.get().addEventHander(eventHandler, receiveCanceled);
-            case LOWEST -> registered = NeoLivingDeathLowest.get().addEventHander(eventHandler, receiveCanceled);
-        }
-        return registered;
+        return getProxy(priority).addEventHander(eventHandler, receiveCanceled);
     }
 
     public static boolean unregister(IEventHandler eventHandler, EventPriority priority, boolean receiveCanceled) {
-        boolean unregistered = false;
-        switch (priority) {
-            case HIGHEST -> unregistered = NeoLivingDeathHighest.get().removeEventHandler(eventHandler, receiveCanceled);
-            case HIGH -> unregistered = NeoLivingDeathHigh.get().removeEventHandler(eventHandler, receiveCanceled);
-            case NORMAL -> unregistered = NeoLivingDeathNormal.get().removeEventHandler(eventHandler, receiveCanceled);
-            case LOW -> unregistered = NeoLivingDeathLow.get().removeEventHandler(eventHandler, receiveCanceled);
-            case LOWEST -> unregistered = NeoLivingDeathLowest.get().removeEventHandler(eventHandler, receiveCanceled);
+        return getProxy(priority).removeEventHandler(eventHandler, receiveCanceled);
+    }
+
+    private static AbstractNeoEventCommon getProxy(EventPriority priority) {
+        return switch (priority) {
+            case HIGHEST -> Highest.INSTANCE;
+            case HIGH -> High.INSTANCE;
+            case NORMAL -> Normal.INSTANCE;
+            case LOW -> Low.INSTANCE;
+            case LOWEST -> Lowest.INSTANCE;
+        };
+    }
+
+    private static abstract class NeoLivingDeathProxy extends AbstractNeoEventCommon {
+        public NeoLivingDeathProxy() {
+            super(EventType.LIVING_DEATH_EVENT);
         }
-        return unregistered;
+
+        @Override
+        protected void registerToNeo() {
+            NeoForge.EVENT_BUS.register(this);
+        }
+
+        @Override
+        protected void unregisterToNeo() {
+            NeoForge.EVENT_BUS.unregister(this);
+        }
+
+        @Override
+        protected NeoEvent getNeoEventType(Event event) {
+            return new NeoLivingDeathEvent(event);
+        }
+
+        protected void handle(LivingDeathEvent event) {
+            super.onEvent(event);
+        }
+    }
+
+    public static class Highest extends NeoLivingDeathProxy {
+        static final Highest INSTANCE = new Highest();
+        @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.HIGHEST, receiveCanceled = true)
+        public void onEvent(LivingDeathEvent e) { handle(e); }
+    }
+
+    public static class High extends NeoLivingDeathProxy {
+        static final High INSTANCE = new High();
+        @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.HIGH, receiveCanceled = true)
+        public void onEvent(LivingDeathEvent e) { handle(e); }
+    }
+
+    public static class Normal extends NeoLivingDeathProxy {
+        static final Normal INSTANCE = new Normal();
+        @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.NORMAL, receiveCanceled = true)
+        public void onEvent(LivingDeathEvent e) { handle(e); }
+    }
+
+    public static class Low extends NeoLivingDeathProxy {
+        static final Low INSTANCE = new Low();
+        @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.LOW, receiveCanceled = true)
+        public void onEvent(LivingDeathEvent e) { handle(e); }
+    }
+
+    public static class Lowest extends NeoLivingDeathProxy {
+        static final Lowest INSTANCE = new Lowest();
+        @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.LOWEST, receiveCanceled = true)
+        public void onEvent(LivingDeathEvent e) { handle(e); }
     }
 }
