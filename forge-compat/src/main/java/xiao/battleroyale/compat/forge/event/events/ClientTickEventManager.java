2da3de90044fd5fb760aa9c54075f7dbd8283011
XiaoColorful <XColor_ful@outlook.com>
2026-01-31 23:40:55 +0800

    Refactor: Merge isomorphic event listeners into manager facades

--- forge-compat/src/main/java/xiao/battleroyale/compat/forge/event/events/ClientTickEventManager.java
+++ forge-compat/src/main/java/xiao/battleroyale/compat/forge/event/events/ClientTickEventManager.java
@@ -1,31 +1,89 @@
 package xiao.battleroyale.compat.forge.event.events;
 
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import xiao.battleroyale.api.event.EventPriority;
+import xiao.battleroyale.api.event.EventType;
 import xiao.battleroyale.api.event.IEventHandler;
+import xiao.battleroyale.compat.forge.event.ForgeClientTickEvent;
+import xiao.battleroyale.compat.forge.event.ForgeEvent;
 
 public class ClientTickEventManager {
 
     public static boolean register(IEventHandler eventHandler, EventPriority priority, boolean receiveCanceled) {
-        boolean registered = false;
-        switch (priority) {
-            case HIGHEST -> registered = ClientTickHighest.get().addEventHander(eventHandler, receiveCanceled);
-            case HIGH -> registered = ClientTickHigh.get().addEventHander(eventHandler,receiveCanceled);
-            case NORMAL -> registered = ClientTickNormal.get().addEventHander(eventHandler, receiveCanceled);
-            case LOW -> registered = ClientTickLow.get().addEventHander(eventHandler, receiveCanceled);
-            case LOWEST -> registered = ClientTickLowest.get().addEventHander(eventHandler, receiveCanceled);
-        }
-        return registered;
+        return getProxy(priority).addEventHander(eventHandler, receiveCanceled);
     }
 
     public static boolean unregister(IEventHandler eventHandler, EventPriority priority, boolean receiveCanceled) {
-        boolean unregistered = false;
-        switch (priority) {
-            case HIGHEST -> unregistered = ClientTickHighest.get().removeEventHandler(eventHandler, receiveCanceled);
-            case HIGH -> unregistered = ClientTickHigh.get().removeEventHandler(eventHandler,receiveCanceled);
-            case NORMAL -> unregistered = ClientTickNormal.get().removeEventHandler(eventHandler, receiveCanceled);
-            case LOW -> unregistered = ClientTickLow.get().removeEventHandler(eventHandler, receiveCanceled);
-            case LOWEST -> unregistered = ClientTickLowest.get().removeEventHandler(eventHandler, receiveCanceled);
+        return getProxy(priority).removeEventHandler(eventHandler, receiveCanceled);
+    }
+
+    private static AbstractEventCommon getProxy(EventPriority priority) {
+        return switch (priority) {
+            case HIGHEST -> Highest.INSTANCE;
+            case HIGH -> High.INSTANCE;
+            case NORMAL -> Normal.INSTANCE;
+            case LOW -> Low.INSTANCE;
+            case LOWEST -> Lowest.INSTANCE;
+        };
+    }
+
+    private static abstract class ClientTickProxy extends AbstractEventCommon {
+        public ClientTickProxy() {
+            super(EventType.CLIENT_TICK_EVENT);
         }
-        return unregistered;
+
+        @Override
+        protected void registerToForge() {
+            MinecraftForge.EVENT_BUS.register(this);
+        }
+
+        @Override
+        protected void unregisterToForge() {
+            MinecraftForge.EVENT_BUS.unregister(this);
+        }
+
+        @Override
+        protected ForgeEvent getForgeEventType(Event event) {
+            return new ForgeClientTickEvent(event);
+        }
+
+        protected void handle(TickEvent.ClientTickEvent event) {
+            if (event.phase == TickEvent.Phase.END) {
+                super.onEvent(event);
+            }
+        }
+    }
+
+    public static class Highest extends ClientTickProxy {
+        static final Highest INSTANCE = new Highest();
+        @SubscribeEvent(priority = net.minecraftforge.eventbus.api.EventPriority.HIGHEST, receiveCanceled = true)
+        public void onEvent(TickEvent.ClientTickEvent e) { handle(e); }
+    }
+
+    public static class High extends ClientTickProxy {
+        static final High INSTANCE = new High();
+        @SubscribeEvent(priority = net.minecraftforge.eventbus.api.EventPriority.HIGH, receiveCanceled = true)
+        public void onEvent(TickEvent.ClientTickEvent e) { handle(e); }
+    }
+
+    public static class Normal extends ClientTickProxy {
+        static final Normal INSTANCE = new Normal();
+        @SubscribeEvent(priority = net.minecraftforge.eventbus.api.EventPriority.NORMAL, receiveCanceled = true)
+        public void onEvent(TickEvent.ClientTickEvent e) { handle(e); }
+    }
+
+    public static class Low extends ClientTickProxy {
+        static final Low INSTANCE = new Low();
+        @SubscribeEvent(priority = net.minecraftforge.eventbus.api.EventPriority.LOW, receiveCanceled = true)
+        public void onEvent(TickEvent.ClientTickEvent e) { handle(e); }
+    }
+
+    public static class Lowest extends ClientTickProxy {
+        static final Lowest INSTANCE = new Lowest();
+        @SubscribeEvent(priority = net.minecraftforge.eventbus.api.EventPriority.LOWEST, receiveCanceled = true)
+        public void onEvent(TickEvent.ClientTickEvent e) { handle(e); }
     }
 }
