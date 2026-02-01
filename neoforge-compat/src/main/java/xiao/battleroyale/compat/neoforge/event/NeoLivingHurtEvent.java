5118284dff703d2d514826bfa3507f9c7db01cfe
XiaoColorful <XColor_ful@outlook.com>
2026-02-01 12:24:43 +0800

    Add setDamageAmount to ILivingHurtEvent (neoforge)

--- neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoLivingHurtEvent.java
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoLivingHurtEvent.java
@@ -34,4 +34,9 @@ public class NeoLivingHurtEvent extends NeoEvent implements ILivingHurtEvent {
     public float getDamageAmount() {
         return livingHurtEvent.getAmount();
     }
+
+    @Override
+    public void setDamageAmount(float amount) {
+        this.livingHurtEvent.setAmount(amount);
+    }
 }
