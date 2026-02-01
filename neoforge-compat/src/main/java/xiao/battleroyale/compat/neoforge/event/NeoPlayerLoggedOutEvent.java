88447c3351389832043b22687391fb3db26e1b20
XiaoColorful <XColor_ful@outlook.com>
2026-02-01 12:08:00 +0800

    Deprecate setCanceled in ILivingDamageEvent (neoforge)

--- neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoPlayerLoggedOutEvent.java
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoPlayerLoggedOutEvent.java
@@ -11,8 +11,8 @@ public class NeoPlayerLoggedOutEvent extends NeoEvent implements IPlayerLoggedOu
 
     public NeoPlayerLoggedOutEvent(Event event) {
         super(event);
-        if (event instanceof PlayerEvent.PlayerLoggedOutEvent playerLoggedOutEvent) {
-            this.playerLoggedOutEvent = playerLoggedOutEvent;
+        if (event instanceof PlayerEvent.PlayerLoggedOutEvent eventIn) {
+            this.playerLoggedOutEvent = eventIn;
         } else {
             throw new RuntimeException("Expected PlayerLoggedOutEvent but received: " + event.getClass().getName());
         }
