6b48e9aac335cdb81977917f4dfc4cc18553c0d1
XiaoColorful <XColor_ful@outlook.com>
2026-02-01 11:39:02 +0800

    Port LivingAttackEvent to neoforge1.21.1

--- neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoLivingAttackEvent.java
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoLivingAttackEvent.java
@@ -3,17 +3,17 @@ package xiao.battleroyale.compat.neoforge.event;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.bus.api.Event;
-import net.neoforged.neoforge.event.entity.living.LivingAttackEvent;
+import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 import org.jetbrains.annotations.NotNull;
 import xiao.battleroyale.api.event.ILivingAttackEvent;
 
 public class NeoLivingAttackEvent extends NeoEvent implements ILivingAttackEvent {
 
-    protected LivingAttackEvent livingAttackEvent;
+    protected LivingIncomingDamageEvent livingAttackEvent;
 
     public NeoLivingAttackEvent(Event event) {
         super(event);
-        if (event instanceof LivingAttackEvent eventIn) {
+        if (event instanceof LivingIncomingDamageEvent eventIn) {
             this.livingAttackEvent = eventIn;
         } else {
             throw new RuntimeException("Expected LivingAttackEvent but received: " + event.getClass().getName());
--- neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/events/NeoLivingAttackEventManager.java
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/events/NeoLivingAttackEventManager.java
@@ -3,7 +3,7 @@ package xiao.battleroyale.compat.neoforge.event.events;
 import net.neoforged.bus.api.Event;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.entity.living.LivingAttackEvent;
+import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 import xiao.battleroyale.api.event.EventPriority;
 import xiao.battleroyale.api.event.EventType;
 import xiao.battleroyale.api.event.IEventHandler;
@@ -39,36 +39,36 @@ public class NeoLivingAttackEventManager {
         @Override protected void unregisterToNeo() { NeoForge.EVENT_BUS.unregister(this); }
         @Override protected NeoEvent getNeoEventType(Event event) { return new NeoLivingAttackEvent(event); }
 
-        protected void handle(LivingAttackEvent event) { super.onEvent(event); }
+        protected void handle(LivingIncomingDamageEvent event) { super.onEvent(event); }
     }
 
     public static class Highest extends NeoLivingAttackProxy {
         static final Highest INSTANCE = new Highest();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.HIGHEST, receiveCanceled = true)
-        public void onEvent(LivingAttackEvent e) { handle(e); }
+        public void onEvent(LivingIncomingDamageEvent e) { handle(e); }
     }
 
     public static class High extends NeoLivingAttackProxy {
         static final High INSTANCE = new High();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.HIGH, receiveCanceled = true)
-        public void onEvent(LivingAttackEvent e) { handle(e); }
+        public void onEvent(LivingIncomingDamageEvent e) { handle(e); }
     }
 
     public static class Normal extends NeoLivingAttackProxy {
         static final Normal INSTANCE = new Normal();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.NORMAL, receiveCanceled = true)
-        public void onEvent(LivingAttackEvent e) { handle(e); }
+        public void onEvent(LivingIncomingDamageEvent e) { handle(e); }
     }
 
     public static class Low extends NeoLivingAttackProxy {
         static final Low INSTANCE = new Low();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.LOW, receiveCanceled = true)
-        public void onEvent(LivingAttackEvent e) { handle(e); }
+        public void onEvent(LivingIncomingDamageEvent e) { handle(e); }
     }
 
     public static class Lowest extends NeoLivingAttackProxy {
         static final Lowest INSTANCE = new Lowest();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.LOWEST, receiveCanceled = true)
-        public void onEvent(LivingAttackEvent e) { handle(e); }
+        public void onEvent(LivingIncomingDamageEvent e) { handle(e); }
     }
 }
