465ffa17520262b3ee9a4c881e42b4e085e20e90
XiaoColorful <XColor_ful@outlook.com>
2026-02-08 17:28:42 +0800

    Port 0.4.8-3 to neoforge1.21.6

--- core/src/main/java/xiao/battleroyale/client/renderer/block/LootContainerRenderer.java
+++ core/src/main/java/xiao/battleroyale/client/renderer/block/LootContainerRenderer.java
@@ -67,7 +67,6 @@ public abstract class LootContainerRenderer<T extends AbstractLootContainerBlock
 
         // 获取摄像机数据
         Camera camera = mc.gameRenderer.getMainCamera();
-        Vec3 cameraPos = camera.getPosition();
         Vector3f cameraLook = camera.getLookVector();
 
         // ---1. 距离判断---
