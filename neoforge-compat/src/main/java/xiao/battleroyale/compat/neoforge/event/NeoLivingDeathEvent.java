1a0e4d21c08fb363e3ef37b6444903433c1e6a01
XiaoColorful <XColor_ful@outlook.com>
2026-02-10 00:53:00 +0800

    Update NeoLivingDeathEvent.java

--- neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoLivingDeathEvent.java
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoLivingDeathEvent.java
@@ -4,6 +4,7 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.neoforged.bus.api.Event;
 import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
+import org.jetbrains.annotations.NotNull;
 import xiao.battleroyale.api.event.ILivingDeathEvent;
 
 public class NeoLivingDeathEvent extends NeoEvent implements ILivingDeathEvent {
