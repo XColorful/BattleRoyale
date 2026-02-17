501ac88501f7c3ac6ebb449a1db8d24895f51228
XiaoColorful <XColor_ful@outlook.com>
2026-02-18 02:42:36 +0800

    Add DeathMatch GameProcessManager

---
+++ core/src/main/java/xiao/battleroyale/api/game/process/deathmatch/IDeathMatchProcessManager.java
@@ -0,0 +1,6 @@
+package xiao.battleroyale.api.game.process.deathmatch;
+
+import xiao.battleroyale.api.game.process.IGameProcessManager;
+
+public interface IDeathMatchProcessManager extends IGameProcessManager, IDeathMatchDataManagement, IDeathMatchGameManagement {
+}
