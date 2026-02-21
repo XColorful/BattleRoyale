85cc189ef1b61d8a5765d6501193348a29d803a5
XiaoColorful <XColor_ful@outlook.com>
2026-02-22 01:12:39 +0800

    Port RenderLevelStageEvent to neoforge1.21.6

--- neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/events/NeoRenderTranslucentEventManager.java
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/events/NeoRenderTranslucentEventManager.java
@@ -4,12 +4,10 @@ import net.neoforged.bus.api.Event;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
 import net.neoforged.neoforge.common.NeoForge;
-import xiao.battleroyale.api.event.RenderLevelStage;
 import xiao.battleroyale.api.event.EventPriority;
 import xiao.battleroyale.api.event.EventType;
 import xiao.battleroyale.api.event.IEventHandler;
 import xiao.battleroyale.compat.neoforge.event.NeoEvent;
-import xiao.battleroyale.compat.neoforge.event.NeoRenderLevelStage;
 import xiao.battleroyale.compat.neoforge.event.NeoRenderLevelStageEvent;
 
 public class NeoRenderTranslucentEventManager {
@@ -49,43 +47,41 @@ public class NeoRenderTranslucentEventManager {
 
         @Override
         protected NeoEvent getNeoEventType(Event event) {
-            return new NeoRenderLevelStageEvent((RenderLevelStageEvent) event);
+            return new NeoRenderLevelStageEvent((RenderLevelStageEvent.AfterTranslucentBlocks) event);
         }
 
-        protected void handle(RenderLevelStageEvent event) {
-            if (NeoRenderLevelStage.fromStage(event.getStage()) == RenderLevelStage.AFTER_TRANSLUCENT_BLOCKS) {
-                super.onEvent(event);
-            }
+        protected void handle(RenderLevelStageEvent.AfterTranslucentBlocks event) {
+            super.onEvent(event);
         }
     }
 
     public static class Highest extends NeoRenderTranslucentProxy {
         static final Highest INSTANCE = new Highest();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.HIGHEST, receiveCanceled = true)
-        public void onEvent(RenderLevelStageEvent e) { handle(e); }
+        public void onEvent(RenderLevelStageEvent.AfterTranslucentBlocks e) { handle(e); }
     }
 
     public static class High extends NeoRenderTranslucentProxy {
         static final High INSTANCE = new High();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.HIGH, receiveCanceled = true)
-        public void onEvent(RenderLevelStageEvent e) { handle(e); }
+        public void onEvent(RenderLevelStageEvent.AfterTranslucentBlocks e) { handle(e); }
     }
 
     public static class Normal extends NeoRenderTranslucentProxy {
         static final Normal INSTANCE = new Normal();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.NORMAL, receiveCanceled = true)
-        public void onEvent(RenderLevelStageEvent e) { handle(e); }
+        public void onEvent(RenderLevelStageEvent.AfterTranslucentBlocks e) { handle(e); }
     }
 
     public static class Low extends NeoRenderTranslucentProxy {
         static final Low INSTANCE = new Low();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.LOW, receiveCanceled = true)
-        public void onEvent(RenderLevelStageEvent e) { handle(e); }
+        public void onEvent(RenderLevelStageEvent.AfterTranslucentBlocks e) { handle(e); }
     }
 
     public static class Lowest extends NeoRenderTranslucentProxy {
         static final Lowest INSTANCE = new Lowest();
         @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.LOWEST, receiveCanceled = true)
-        public void onEvent(RenderLevelStageEvent e) { handle(e); }
+        public void onEvent(RenderLevelStageEvent.AfterTranslucentBlocks e) { handle(e); }
     }
 }
