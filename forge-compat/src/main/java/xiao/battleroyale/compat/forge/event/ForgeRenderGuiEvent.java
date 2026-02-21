c636c9036942bcdf2c8fe2d0c5a9bb77de06ef6c
XiaoColorful <XColor_ful@outlook.com>
2026-02-22 00:52:46 +0800

    Port RenderGuiEvent to forge1.21.1

--- forge-compat/src/main/java/xiao/battleroyale/compat/forge/event/ForgeRenderGuiEvent.java
+++ forge-compat/src/main/java/xiao/battleroyale/compat/forge/event/ForgeRenderGuiEvent.java
@@ -1,17 +1,17 @@
 package xiao.battleroyale.compat.forge.event;
 
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraftforge.client.event.RenderGuiEvent;
+import net.minecraftforge.client.event.CustomizeGuiOverlayEvent;
 import net.minecraftforge.eventbus.api.Event;
 import xiao.battleroyale.api.event.IRenderGuiEvent;
 
 public class ForgeRenderGuiEvent extends ForgeEvent implements IRenderGuiEvent {
 
-    private final RenderGuiEvent.Post event;
+    private final CustomizeGuiOverlayEvent event;
 
     public ForgeRenderGuiEvent(Event event) {
         super(event);
-        if (event instanceof RenderGuiEvent.Post eventIn) {
+        if (event instanceof CustomizeGuiOverlayEvent eventIn) {
             this.event = eventIn;
         } else {
             throw new RuntimeException("Expected RenderGuiEvent but received: " + event.getClass().getName());
