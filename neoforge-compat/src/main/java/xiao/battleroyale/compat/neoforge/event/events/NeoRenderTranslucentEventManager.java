a455ba65a5249a09d6e32a08c7ac27dc062deab7
XiaoColorful <XColor_ful@outlook.com>
2026-02-22 00:41:15 +0800

    Port event refactor to neoforge1.20.2

---
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/events/NeoRenderTranslucentEventManager.java
@@ -0,0 +1,91 @@
+package xiao.battleroyale.compat.neoforge.event.events;
+
+import net.neoforged.bus.api.Event;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import xiao.battleroyale.api.event.RenderLevelStage;
+import xiao.battleroyale.api.event.EventPriority;
+import xiao.battleroyale.api.event.EventType;
+import xiao.battleroyale.api.event.IEventHandler;
+import xiao.battleroyale.compat.neoforge.event.NeoEvent;
+import xiao.battleroyale.compat.neoforge.event.NeoRenderLevelStage;
+import xiao.battleroyale.compat.neoforge.event.NeoRenderLevelStageEvent;
+
+public class NeoRenderTranslucentEventManager {
+
+    public static boolean register(IEventHandler eventHandler, EventPriority priority, boolean receiveCanceled) {
+        return getProxy(priority).addEventHander(eventHandler, receiveCanceled);
+    }
+
+    public static boolean unregister(IEventHandler eventHandler, EventPriority priority, boolean receiveCanceled) {
+        return getProxy(priority).removeEventHandler(eventHandler, receiveCanceled);
+    }
+
+    private static AbstractNeoEventCommon getProxy(EventPriority priority) {
+        return switch (priority) {
+            case HIGHEST -> Highest.INSTANCE;
+            case HIGH -> High.INSTANCE;
+            case NORMAL -> Normal.INSTANCE;
+            case LOW -> Low.INSTANCE;
+            case LOWEST -> Lowest.INSTANCE;
+        };
+    }
+
+    private static abstract class NeoRenderTranslucentProxy extends AbstractNeoEventCommon {
+        public NeoRenderTranslucentProxy() {
+            super(EventType.RENDER_TRANSLUCENT_EVENT);
+        }
+
+        @Override
+        protected void registerToNeo() {
+            NeoForge.EVENT_BUS.register(this);
+        }
+
+        @Override
+        protected void unregisterToNeo() {
+            NeoForge.EVENT_BUS.unregister(this);
+        }
+
+        @Override
+        protected NeoEvent getNeoEventType(Event event) {
+            return new NeoRenderLevelStageEvent((RenderLevelStageEvent) event);
+        }
+
+        protected void handle(RenderLevelStageEvent event) {
+            if (NeoRenderLevelStage.fromStage(event.getStage()) == RenderLevelStage.AFTER_TRANSLUCENT_BLOCKS) {
+                super.onEvent(event);
+            }
+        }
+    }
+
+    public static class Highest extends NeoRenderTranslucentProxy {
+        static final Highest INSTANCE = new Highest();
+        @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.HIGHEST, receiveCanceled = true)
+        public void onEvent(RenderLevelStageEvent e) { handle(e); }
+    }
+
+    public static class High extends NeoRenderTranslucentProxy {
+        static final High INSTANCE = new High();
+        @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.HIGH, receiveCanceled = true)
+        public void onEvent(RenderLevelStageEvent e) { handle(e); }
+    }
+
+    public static class Normal extends NeoRenderTranslucentProxy {
+        static final Normal INSTANCE = new Normal();
+        @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.NORMAL, receiveCanceled = true)
+        public void onEvent(RenderLevelStageEvent e) { handle(e); }
+    }
+
+    public static class Low extends NeoRenderTranslucentProxy {
+        static final Low INSTANCE = new Low();
+        @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.LOW, receiveCanceled = true)
+        public void onEvent(RenderLevelStageEvent e) { handle(e); }
+    }
+
+    public static class Lowest extends NeoRenderTranslucentProxy {
+        static final Lowest INSTANCE = new Lowest();
+        @SubscribeEvent(priority = net.neoforged.bus.api.EventPriority.LOWEST, receiveCanceled = true)
+        public void onEvent(RenderLevelStageEvent e) { handle(e); }
+    }
+}
