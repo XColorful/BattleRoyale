9788065af27f1061efa4025876b646137e3a98c7
XiaoColorful <XColor_ful@outlook.com>
2026-03-19 18:27:22 +0800

    Add event type to neoforge event

--- neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoClientTickEvent.java
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoClientTickEvent.java
@@ -1,7 +1,12 @@
 package xiao.battleroyale.compat.neoforge.event;
 
+import net.minecraft.commands.CommandSource;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
 import net.neoforged.bus.api.Event;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
+import org.jetbrains.annotations.Nullable;
+import xiao.battleroyale.api.event.EventType;
 import xiao.battleroyale.api.event.IClientTickEvent;
 
 public class NeoClientTickEvent extends NeoEvent implements IClientTickEvent {
@@ -15,5 +20,20 @@ public class NeoClientTickEvent extends NeoEvent implements IClientTickEvent {
         } else {
             throw new RuntimeException("Expected ClientTickEvent but received: " + event.getClass().getName());
         }
+    }
+    @Override public EventType getType() {
+        return EventType.CLIENT_TICK_EVENT;
+    }
+
+    @Override
+    public @Nullable CommandSourceStack createCommandSourceStack(@Nullable CommandSource source) {
+        return null;
+    }
+
+    @Override public String getTextName() {
+        return "NeoClientTickEvent";
+    }
+    @Override public Component getDisplayName() {
+        return Component.literal(getTextName());
     }
 }
--- neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoRenderLevelStageEvent.java
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoRenderLevelStageEvent.java
@@ -1,8 +1,13 @@
 package xiao.battleroyale.compat.neoforge.event;
 
+import net.minecraft.commands.CommandSource;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.phys.Vec3;
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
+import org.jetbrains.annotations.Nullable;
 import org.joml.Matrix4f;
+import xiao.battleroyale.api.event.EventType;
 import xiao.battleroyale.api.event.IRenderLevelStageEvent;
 import xiao.battleroyale.api.event.RenderLevelStage;
 
@@ -15,6 +20,9 @@ public class NeoRenderLevelStageEvent extends NeoEvent implements IRenderLevelSt
         super(event);
         this.typedEvent = event;
         this.stage = NeoRenderLevelStage.fromStage(event.getStage());
+    }
+    @Override public EventType getType() {
+        return EventType.RENDER_LEVEL_STAGE_EVENT;
     }
 
     @Override
@@ -35,5 +43,17 @@ public class NeoRenderLevelStageEvent extends NeoEvent implements IRenderLevelSt
     @Override
     public float getPartialTick() {
         return this.typedEvent.getPartialTick().getGameTimeDeltaPartialTick(true);
+    }
+
+    @Override
+    public @Nullable CommandSourceStack createCommandSourceStack(@Nullable CommandSource source) {
+        return null;
+    }
+
+    @Override public String getTextName() {
+        return "NeoRenderLevelStageEvent";
+    }
+    @Override public Component getDisplayName() {
+        return Component.literal(getTextName());
     }
 }
