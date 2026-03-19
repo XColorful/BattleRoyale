ffd0f5fa58c94dcd7a25f537b2e5de2d96a6df9e
XiaoColorful <XColor_ful@outlook.com>
2026-03-19 18:27:22 +0800

    Add event type to neoforge event

--- neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoLivingHurtEvent.java
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoLivingHurtEvent.java
@@ -1,10 +1,17 @@
 package xiao.battleroyale.compat.neoforge.event;
 
+import net.minecraft.commands.CommandSource;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.phys.Vec2;
 import net.neoforged.bus.api.Event;
 import net.neoforged.neoforge.event.entity.living.LivingDamageEvent;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import xiao.battleroyale.api.event.EventType;
 import xiao.battleroyale.api.event.ILivingHurtEvent;
 
 public class NeoLivingHurtEvent extends NeoEvent implements ILivingHurtEvent {
@@ -18,6 +25,9 @@ public class NeoLivingHurtEvent extends NeoEvent implements ILivingHurtEvent {
         } else {
             throw new RuntimeException("Expected LivingHurtEvent but received: " + event.getClass().getName());
         }
+    }
+    @Override public EventType getType() {
+        return EventType.LIVING_HURT_EVENT;
     }
 
     @Override
@@ -38,5 +48,28 @@ public class NeoLivingHurtEvent extends NeoEvent implements ILivingHurtEvent {
     @Override
     public void setDamageAmount(float amount) {
         livingHurtEvent.setNewDamage(amount);
+    }
+
+    @Override
+    public @Nullable CommandSourceStack createCommandSourceStack(@Nullable CommandSource source) {
+        @NotNull LivingEntity entity = this.getEntity();
+        return new CommandSourceStack(
+                source != null ? source : CommandSource.NULL,
+                entity.position(),
+                Vec2.ZERO,
+                (ServerLevel) entity.level(),
+                4,
+                this.getTextName(),
+                this.getDisplayName(),
+                entity.getServer(),
+                entity
+        );
+    }
+
+    @Override public String getTextName() {
+        return this.getEntity().getName().getString();
+    }
+    @Override public Component getDisplayName() {
+        return this.getEntity().getDisplayName();
     }
 }
