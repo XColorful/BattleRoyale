07a2c87ed93bdc818305c69b2d547e0d9cd5d543
XiaoColorful <XColor_ful@outlook.com>
2026-03-19 18:27:22 +0800

    Add event type to neoforge event

--- neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoLivingAttackEvent.java
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoLivingAttackEvent.java
@@ -1,10 +1,17 @@
 package xiao.battleroyale.compat.neoforge.event;
 
+import net.minecraft.commands.CommandSource;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.phys.Vec2;
 import net.neoforged.bus.api.Event;
 import net.neoforged.neoforge.event.entity.living.LivingAttackEvent;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import xiao.battleroyale.api.event.EventType;
 import xiao.battleroyale.api.event.ILivingAttackEvent;
 
 public class NeoLivingAttackEvent extends NeoEvent implements ILivingAttackEvent {
@@ -18,6 +25,9 @@ public class NeoLivingAttackEvent extends NeoEvent implements ILivingAttackEvent
         } else {
             throw new RuntimeException("Expected LivingAttackEvent but received: " + event.getClass().getName());
         }
+    }
+    @Override public EventType getType() {
+        return EventType.LIVING_ATTACK_EVENT;
     }
 
     @Override
@@ -33,5 +43,28 @@ public class NeoLivingAttackEvent extends NeoEvent implements ILivingAttackEvent
     @Override
     public float getDamageAmount() {
         return livingAttackEvent.getAmount();
+    }
+
+    @Override
+    public @Nullable CommandSourceStack createCommandSourceStack(@Nullable CommandSource source) {
+        @NotNull LivingEntity entity = this.getEntity();
+        return new CommandSourceStack(
+                source != null ? source : CommandSource.NULL,
+                entity.position(),
+                Vec2.ZERO,
+                (ServerLevel) entity.level(),
+                4,
+                this.getTextName(),
+                this.getDisplayName(),
+                entity.getServer(),
+                entity
+        );
+    }
+
+    @Override public String getTextName() {
+        return this.getEntity().getName().getString();
+    }
+    @Override public Component getDisplayName() {
+        return this.getEntity().getDisplayName();
     }
 }
--- neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoServerTickEvent.java
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoServerTickEvent.java
@@ -1,7 +1,16 @@
 package xiao.battleroyale.compat.neoforge.event;
 
+import net.minecraft.commands.CommandSource;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.phys.Vec3;
 import net.neoforged.bus.api.Event;
 import net.neoforged.neoforge.event.TickEvent;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import xiao.battleroyale.api.event.EventType;
 import xiao.battleroyale.api.event.IServerTickEvent;
 
 public class NeoServerTickEvent extends NeoEvent implements IServerTickEvent {
@@ -15,5 +24,36 @@ public class NeoServerTickEvent extends NeoEvent implements IServerTickEvent {
         } else {
             throw new RuntimeException("Expected ServerTickEvent but received: " + event.getClass().getName());
         }
+    }
+    @Override public EventType getType() {
+        return EventType.SERVER_TICK_EVENT;
+    }
+
+    @Override
+    public MinecraftServer getServer() {
+        return serverTickEvent.getServer();
+    }
+
+    @Override
+    public @NotNull CommandSourceStack createCommandSourceStack(@Nullable CommandSource source) {
+        MinecraftServer server = this.getServer();
+        return new CommandSourceStack(
+                source != null ? source : CommandSource.NULL,
+                Vec3.ZERO,
+                Vec2.ZERO,
+                server.overworld(),
+                4,
+                this.getTextName(),
+                this.getDisplayName(),
+                server,
+                null
+        );
+    }
+
+    @Override public String getTextName() {
+        return "NeoServerTickEvent";
+    }
+    @Override public Component getDisplayName() {
+        return Component.literal(getTextName());
     }
 }
