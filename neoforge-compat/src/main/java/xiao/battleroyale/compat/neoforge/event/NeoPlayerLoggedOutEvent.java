c5a87e23d38d9e85d1196b2ba0bffe39b860e2e9
XiaoColorful <XColor_ful@outlook.com>
2026-03-20 00:26:47 +0800

    Port permission level refactor to neoforge

--- neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoPlayerLoggedOutEvent.java
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoPlayerLoggedOutEvent.java
@@ -13,6 +13,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import xiao.battleroyale.api.event.EventType;
 import xiao.battleroyale.api.event.IPlayerLoggedOutEvent;
+import xiao.battleroyale.api.minecraft.CommandLevel;
 
 public class NeoPlayerLoggedOutEvent extends NeoEvent implements IPlayerLoggedOutEvent {
 
@@ -43,7 +44,7 @@ public class NeoPlayerLoggedOutEvent extends NeoEvent implements IPlayerLoggedOu
                 entity.position(),
                 Vec2.ZERO,
                 (ServerLevel) entity.level(),
-                4,
+                CommandLevel.permission(4),
                 this.getTextName(),
                 this.getDisplayName(),
                 entity.level().getServer(),
