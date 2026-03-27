9cc51c77e859d4390a7f4d4ff126d56005967f4a
XiaoColorful <XColor_ful@outlook.com>
2026-03-27 16:08:57 +0800

    Add BlockEvent, PlayerInteractEvent (neoforge)

---
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/minecraft/TriResultHelper.java
@@ -0,0 +1,22 @@
+package xiao.battleroyale.compat.neoforge.minecraft;
+
+import net.neoforged.bus.api.Event;
+import xiao.battleroyale.api.minecraft.TriResult;
+
+public class TriResultHelper {
+
+    public static TriResult convert(Event.Result result) {
+        return switch (result) {
+            case ALLOW -> TriResult.ALLOW;
+            case DENY -> TriResult.DENY;
+            default -> TriResult.DEFAULT;
+        };
+    }
+    public static Event.Result convert(TriResult result) {
+        return switch (result) {
+            case ALLOW -> Event.Result.ALLOW;
+            case DENY -> Event.Result.DENY;
+            default -> Event.Result.DEFAULT;
+        };
+    }
+}
