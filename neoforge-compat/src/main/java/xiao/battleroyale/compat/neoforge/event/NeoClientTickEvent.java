52b5fb6d3cfe99a441a6747baf90e2e5560b0da1
XiaoColorful <XColor_ful@outlook.com>
2026-03-27 15:59:58 +0800

    Update NeoClientTickEvent.java

--- neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoClientTickEvent.java
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoClientTickEvent.java
@@ -15,8 +15,8 @@ public class NeoClientTickEvent extends NeoEvent implements IClientTickEvent {
 
     public NeoClientTickEvent(Event event) {
         super(event);
-        if (event instanceof ClientTickEvent.Post clientTickEvent) {
-            this.clientTickEvent = clientTickEvent;
+        if (event instanceof ClientTickEvent.Post eventIn) {
+            this.clientTickEvent = eventIn;
         } else {
             throw new RuntimeException("Expected ClientTickEvent but received: " + event.getClass().getName());
         }
