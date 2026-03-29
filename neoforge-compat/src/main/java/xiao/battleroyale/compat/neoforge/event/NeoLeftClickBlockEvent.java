1160aa52258231cc3abfba94f04b9f72091dfd4f
XiaoColorful <XColor_ful@outlook.com>
2026-03-29 20:10:43 +0800

    Add main hand filter to PlayerInteractEvents for CommandSourceStack (neoforge)

--- neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoLeftClickBlockEvent.java
+++ neoforge-compat/src/main/java/xiao/battleroyale/compat/neoforge/event/NeoLeftClickBlockEvent.java
@@ -102,6 +102,7 @@ public class NeoLeftClickBlockEvent extends NeoEvent implements ILeftClickBlockE
 
     @Override
     public @Nullable CommandSourceStack createCommandSourceStack(@Nullable CommandSource source) {
+        if (getHand() != InteractionHand.MAIN_HAND) return null; // 只给 function 传主手触发的事件
         Level level = getLevel();
         if (level != null && level.isClientSide()) return null;
         return new CommandSourceStack(
